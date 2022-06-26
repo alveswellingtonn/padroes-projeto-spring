04a0748d1b2a98d94df169a164bfdffc3722cc99
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2022-06-26 20:00:40 -0300

    Padrões de projetos com Spring

---
+++ lab-padroes-projeto-spring/src/main/java/wellington/labpadroesprojetospring/service/ClienteService.java
@@ -0,0 +1,17 @@
+package wellington.labpadroesprojetospring.service;
+
+import wellington.labpadroesprojetospring.model.Cliente;
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
